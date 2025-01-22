19cade249df6c4b823a3e7d37541fce533b0a1ab
kuberseth002 <kuber0804.be21@chitkara.edu.in>
2025-01-22 23:10:10 +0530

    kuber Add: use case 1 of address book

---
+++ AddressBook.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var contacts = [];
+var contact = {
+    fName: "kuber",
+    lName: "Seth",
+    address: "123 Model Town",
+    city: "Patiala",
+    state: "Punjab",
+    zip: "1234",
+    phoneNumber: "79541365",
+    email: "kuber@gmial.com"
+};
+contacts.push(contact);
+console.log(contact);
---
+++ AddressBook.ts
@@ -0,0 +1,30 @@
+// use case 1.
+import * as readlineSync from 'readline-sync';
+
+interface Data {
+  fName:string;
+  lName:String;
+  address:string;
+  city:string;
+  state:string;
+  zip:string;
+  phoneNumber:string;
+  email:string
+}
+
+const contacts:Data[]=[]
+
+const contact:Data={
+  fName:"kuber",
+  lName:"Seth",
+  address:"123 Model Town",
+  city:"Patiala",
+  state:"Punjab",
+  zip:"1234",
+  phoneNumber:"79541365",
+  email:"kuber@gmial.com"
+}
+
+contacts.push(contact)
+
+console.log(contact);

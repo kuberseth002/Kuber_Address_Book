ec001271fd1c6f76c71fc3b1fd8144c4ba472ced
kuberseth002 <kuber0804.be21@chitkara.edu.in>
2025-01-23 10:05:13 +0530

    kuber Add: use case 1

--- AddressBook.ts
+++ AddressBook.ts
@@ -1,33 +1,29 @@
 // use case 1.
 import * as readlineSync from 'readline-sync';
 
-interface Data {
-  fName:string;
-  lName:String;
-  address:string;
-  city:string;
-  state:string;
-  zip:string;
-  phoneNumber:string;
-  email:string
-}
+ interface Data {
+   fName:string;
+   lName:String;
+   address:string;
+   city:string;
+   state:string;
+   zip:string;
+   phoneNumber:string;
+   email:string
+ }
+ const contacts:Data[]=[]
+ const contact:Data={
+   fName:"kuber",
+   lName:"Seth",
+   address:"123 Model Town",
+   city:"Patiala",
+   state:"Punjab",
+   zip:"1234",
+   phoneNumber:"79541365",
+   email:"kuber@gmial.com"
+ }
 
-const contacts:Data[]=[]
-
-const contact:Data={
-  fName:"kuber",
-  lName:"Seth",
-  address:"123 Model Town",
-  city:"Patiala",
-  state:"Punjab",
-  zip:"1234",
-  phoneNumber:"79541365",
-  email:"kuber@gmial.com"
-}
-
-contacts.push(contact)
-
-console.log(contact);
+ contacts.push(contact)
+ console.log(contact);
 
 
-//Use Case 2

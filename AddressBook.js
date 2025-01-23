54226ddb43f4fa4a78fad947e9a6e1af7cd7c6e7
kuberseth002 <kuber0804.be21@chitkara.edu.in>
2025-01-23 10:10:31 +0530

    kuber Add: use case 2

--- AddressBook.js
+++ AddressBook.js
@@ -1,15 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var contacts = [];
-var contact = {
-    fName: "kuber",
-    lName: "Seth",
-    address: "123 Model Town",
-    city: "Patiala",
-    state: "Punjab",
-    zip: "1234",
-    phoneNumber: "79541365",
-    email: "kuber@gmial.com"
-};
-contacts.push(contact);
-console.log(contact);
+function addContact() {
+    var fName = readline.question('Enter First Name: ');
+    var lName = readline.question('Enter Last Name: ');
+    var address = readline.question('Enter Address: ');
+    var city = readline.question('Enter City: ');
+    var state = readline.question('Enter State: ');
+    var zip = readline.question('Enter Zip Code: ');
+    var phoneNumber = readline.question('Enter Phone Number: ');
+    var email = readline.question('Enter Email Address: ');
+    var newContact = {
+        fName: fName,
+        lName: lName,
+        address: address,
+        city: city,
+        state: state,
+        zip: zip,
+        phoneNumber: phoneNumber,
+        email: email,
+    };
+    return newContact;
+}
+var newContact = addContact();
+contacts.push(newContact);
+console.log('Contact added successfully!');
+console.log(newContact);
--- AddressBook.ts
+++ AddressBook.ts
@@ -1,30 +1,76 @@
-// use case 1.
+// // use case 1.
 import * as readlineSync from 'readline-sync';
 
+// interface Data {
+//   fName:string;
+//   lName:String;
+//   address:string;
+//   city:string;
+//   state:string;
+//   zip:string;
+//   phoneNumber:string;
+//   email:string
+// }
+
+// const contacts:Data[]=[]
+
+// const contact:Data={
+//   fName:"kuber",
+//   lName:"Seth",
+//   address:"123 Model Town",
+//   city:"Patiala",
+//   state:"Punjab",
+//   zip:"1234",
+//   phoneNumber:"79541365",
+//   email:"kuber@gmial.com"
+// }
+
+// contacts.push(contact)
+
+// console.log(contact);
+
+
+//Use Case 2
+
 interface Data {
-  fName:string;
-  lName:String;
-  address:string;
-  city:string;
-  state:string;
-  zip:string;
-  phoneNumber:string;
-  email:string
+  fName: string;
+  lName: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  phoneNumber: string;
+  email: string;
 }
 
-const contacts:Data[]=[]
+const contacts: Data[] = [];
 
-const contact:Data={
-  fName:"kuber",
-  lName:"Seth",
-  address:"123 Model Town",
-  city:"Patiala",
-  state:"Punjab",
-  zip:"1234",
-  phoneNumber:"79541365",
-  email:"kuber@gmial.com"
+function addContact(): Data {
+  const fName = readline.question('Enter First Name: ');
+  const lName = readline.question('Enter Last Name: ');
+  const address = readline.question('Enter Address: ');
+  const city = readline.question('Enter City: ');
+  const state = readline.question('Enter State: ');
+  const zip = readline.question('Enter Zip Code: ');
+  const phoneNumber = readline.question('Enter Phone Number: ');
+  const email = readline.question('Enter Email Address: ');
+
+  const newContact: Data = {
+    fName,
+    lName,
+    address,
+    city,
+    state,
+    zip,
+    phoneNumber,
+    email,
+  };
+
+  return newContact;
 }
 
-contacts.push(contact)
+const newContact = addContact();
+contacts.push(newContact);
 
-console.log(contact);
+console.log('Contact added successfully!');
+console.log(newContact);
